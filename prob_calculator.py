f31ed3668355af6115dbfe42d323f538fb15245e
kkyouma <144501527+kkyouma@users.noreply.github.com>
2024-02-17 08:52:16 -0300

    add experiment function

--- prob_calculator.py
+++ prob_calculator.py
@@ -8,7 +8,7 @@ class Hat:
     def __init__(self, **kwargs):
         self.contents = []
         for color, quantity in kwargs.items():
-            self.balls.extend([color] * quantity)
+            self.contents.extend([color] * quantity)
 
     def draw(self, num_balls):
         drawn_balls = []
@@ -23,4 +23,20 @@ class Hat:
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
+    count_success = 0
+
+    for _ in range(num_experiments):
+        hat_copy = copy.deepcopy(hat)
+        drawn_balls = hat_copy.draw(num_balls_drawn)
+        drawn_dict = {color: drawn_balls.count(color) for color in set(drawn_balls)}
+
+        success = True
+        for color, quantity in expected_balls.items():
+            if drawn_dict.get(color, 0) < quantity:
+                success = False
+                break
+
+        if success:
+            count_success += 1
+
+    return count_success / num_experiments
