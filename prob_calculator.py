79d880114a0d31606dd5e479a2fb36256b3c2a49
kkyouma <144501527+kkyouma@users.noreply.github.com>
2024-02-17 07:00:16 -0300

    add draw method

--- prob_calculator.py
+++ prob_calculator.py
@@ -6,9 +6,20 @@ import random
 
 class Hat:
     def __init__(self, **kwargs):
-        self.balls = []
+        self.contents = []
         for color, quantity in kwargs.items():
             self.balls.extend([color] * quantity)
+
+    def draw(self, num_balls):
+        drawn_balls = []
+        if num_balls >= len(self.contents):
+            drawn_balls += self.contents
+            self.contents = []
+        else:
+            drawn_balls += random.sample(self.contents, num_balls)
+            for ball in drawn_balls:
+                self.contents.remove(ball)
+        return drawn_balls
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
