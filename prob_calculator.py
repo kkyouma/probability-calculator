44903f7b95afa7fceedeb8eb0a533c1efbb40fe9
kkyouma <144501527+kkyouma@users.noreply.github.com>
2024-02-17 06:38:37 -0300

    add constructor to hat class

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,8 +1,15 @@
 import copy
 import random
+
 # Consider using the modules imported above.
 
+
 class Hat:
+    def __init__(self, **kwargs):
+        self.balls = []
+        for color, quantity in kwargs.items():
+            self.balls.extend([color] * quantity)
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
